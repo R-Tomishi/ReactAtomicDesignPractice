263aeac2354870497d4f4155ecf3436891f9b2cf
R-Tomishi <43924379+R-Tomishi@users.noreply.github.com>
2022-04-03 10:36:21 +0900

    finish1

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,14 +1,23 @@
 import styled from "styled-components";
+import React, { useContext, memo } from "react";
+import { UserContext } from "../../../providers/UserProvider";
+import { useRecoilValue } from "recoil";
+import { userState } from "../../../store/userState";
 
-export const UserIConWithName = (props) => {
+export const UserIConWithName = memo((props) => {
   const { image, name } = props;
+  // const { userInfo } = useContext(UserContext);
+  const userInfo = useRecoilValue(userState);
+  const isAdmin = userInfo ? userInfo.isAdmin : false;
+
   return (
     <SContainer>
       <SImg height={160} width={160} src={image} alt={name} />
       <SName>{name}</SName>
+      {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   );
-};
+});
 
 const SContainer = styled.div`
   text-align: center;
@@ -16,10 +25,14 @@ const SContainer = styled.div`
 const SImg = styled.img`
   border-radius: 50%;
 `;
-
 const SName = styled.p`
   font-size: 18px;
   font-weight: bold;
   margin: 0px;
   color: #40514e;
+`;
+const SEdit = styled.span`
+  text-decoration: underline;
+  color: #aaa;
+  cursor: pointer;
 `;
--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { Card } from "../../atoms/card/card";
 import { UserIConWithName } from "../../molecules/user/UserIconWithName";
+import { memo } from "react";
 
-export const UserCard = (props) => {
+export const UserCard = memo((props) => {
   const { user } = props;
   return (
     <Card>
@@ -19,7 +20,7 @@ export const UserCard = (props) => {
       </SDl>
     </Card>
   );
-};
+});
 
 const SDl = styled.dl`
   text-align: left;
